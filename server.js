635dc24efa58c58c7fe83fc0c774795feafb01d0
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-07-30 21:13:20 -0400

    forked, cloned, added dependencies, got server up and running

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
+
+server.use(express.json());
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +11,7 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`${req.method}, ${req.url}`)
 };
 
 module.exports = server;
