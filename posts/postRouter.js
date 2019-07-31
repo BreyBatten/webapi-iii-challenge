00b22d2075f37830cd89d6e692bc1c801bb4747b
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-07-30 23:42:03 -0400

    finished writing requests in userRouter.js and creating middleware. Starting postRouter

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,9 +1,17 @@
-const express = 'express';
+const express = require('express');
+
+const Posts = require("./postDb");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.get(req.query);
+        res.status(200).json(posts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Error retrieving the posts" });
+    }
 });
 
 router.get('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
 server.use(express.json());
+server.use(logger);
+
+server.use("/api/users", userRouter);
+// server.use("/api/posts", postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -11,7 +16,9 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-  console.log(`${req.method}, ${req.url}`)
+  const requestTime = new Date();
+  console.log(`${req.method}, ${req.url}, ${requestTime}`);
+  next();
 };
 
 module.exports = server;
